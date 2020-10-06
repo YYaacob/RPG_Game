d530de49185f851579777f5fc6a2424682180a7f
Yaacob <150936@student.horizoncollege.nl>
2020-10-06 12:07:08 +0200

    added the beginning of inventory system, inventory textures still missing

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -7,16 +7,13 @@ public class Inventory : MonoBehaviour
 
     public GameObject invTab;
 
-    // Use this for initialization
     void Start()
     {
         invTab.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         if (Input.GetKey(KeyCode.Tab))
         {
             invTab.SetActive(true);
