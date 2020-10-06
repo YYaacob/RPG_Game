c18ca6a6fa71faf4b4f295ffac2741d76b40b33d
Yaacob <150936@student.horizoncollege.nl>
2020-10-06 22:07:42 +0200

    remov not working code

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -11,53 +11,5 @@ public class Item : MonoBehaviour
 
     [HideInInspector]
     public bool pickedUp;
-
-    [HideInInspector]
-    public bool equipped;
-
-    [HideInInspector]
-    public GameObject weaponManager;
     
-    [HideInInspector]
-    public GameObject weapon;
-
-    public bool playersWeapon;
-
-    private void Start()
-    {
-        weaponManager = GameObject.FindWithTag("WeaponManager");
-
-        if (!playersWeapon)
-        {
-            int allWeapons = weaponManager.transform.childCount;
-
-            for (int i = 0; i < allWeapons; i++)
-            {
-                if (weaponManager.transform.GetChild(i).gameObject.GetComponent<Item>().ID == ID)
-                {
-                    weapon = weaponManager.transform.GetChild(i).gameObject;
-                }
-            }
-        }
-    }
-    private void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            equipped = false;
-        }
-        if(equipped == false)
-        {
-            gameObject.SetActive(false);
-        }
-    }
-
-    public void ItemUsage()
-    {
-        if (type == "Weapon")
-        {
-            weapon.SetActive(true);
-            weapon.GetComponent<Item>().equipped = true;
-        }
-    }
 }
--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -1,10 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class Slot : MonoBehaviour, IPointerClickHandler
+public class Slot : MonoBehaviour
 {
     public GameObject item;
     public int ID;
@@ -25,13 +24,4 @@ public class Slot : MonoBehaviour, IPointerClickHandler
         slotIconGameObject.GetComponent<Image>().sprite = icon;
     }
 
-    public void UseItem()
-    {
-        item.GetComponent<Item>().ItemUsage();
-    }
-
-    public void OnPointerClick(PointerEventData pointerEventData)
-    {
-        UseItem();
-    }
 }
